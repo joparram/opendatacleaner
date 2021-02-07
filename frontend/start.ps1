6f2aa05aebaf3c3b8d081386f42fcdc767bc3fcb
José Manuel <parra.martin.josemanuel@gmail.com>
2021-02-07 22:23:56 +0100

    [FIX] frontstart script

--- frontend/start.ps1
+++ frontend/start.ps1
@@ -6,5 +6,5 @@ try {
     iex "docker stop odc-frontend"
     iex "docker rm odc-frontend"
 } catch {}
-iex "docker run --rm -d -p 3000:80 --NODE_ENV=$NODE_ENV --name odc-frontend odc-frontend:${VERSION}.${COMMIT}"
+iex "docker run --rm -d -p 3000:80 --env NODE_ENV=$NODE_ENV --name odc-frontend odc-frontend:${VERSION}.${COMMIT}"
 docker logs -f odc-frontend
