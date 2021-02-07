76b78e12e0165f28761899164c71abff0271b0fd
José Manuel <parra.martin.josemanuel@gmail.com>
2021-02-07 22:11:51 +0100

    [ADD] update start script in front app

--- frontend/start.ps1
+++ frontend/start.ps1
@@ -1,9 +1,10 @@
 $COMMIT=$(git rev-parse HEAD)
 $VERSION=$(cat VERSION)
+$NODE_ENV="production"
 iex "docker build . --tag odc-frontend:${VERSION}.${COMMIT}"
 try {
     iex "docker stop odc-frontend"
     iex "docker rm odc-frontend"
 } catch {}
-iex "docker run --rm -d -p 4200:4200 --name odc-frontend odc-frontend:${VERSION}.${COMMIT}"
+iex "docker run --rm -d -p 3000:80 --NODE_ENV=$NODE_ENV --name odc-frontend odc-frontend:${VERSION}.${COMMIT}"
 docker logs -f odc-frontend
