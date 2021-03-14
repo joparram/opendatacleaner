adf6838b67020e439ef9ce367eba698f9d332c47
José Manuel <parra.martin.josemanuel@gmail.com>
2021-03-14 12:00:27 +0100

    [ADD] update values onCellChange events

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -10,6 +10,8 @@ import { AgGridModule } from 'ag-grid-angular';
 import { ImportService } from '@shared/services/import.service';
 import { ImportDialogComponent } from './import-dialog/import-dialog.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ExDatatableModule } from '@app/ex-datatable/ex-datatable.module';
+
 @NgModule({
   imports: [
     CommonModule,
@@ -21,6 +23,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AgGridModule.withComponents([]),
     FormsModule,
     ReactiveFormsModule,
+    ExDatatableModule,
   ],
   declarations: [HomeComponent, ImportDialogComponent],
   providers: [ImportService],
