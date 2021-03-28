70cb861ad562bfa181a647168eb1cf9f5e9b7365
José Manuel <parra.martin.josemanuel@gmail.com>
2021-03-28 17:58:18 +0200

    [ADD] exporter component which allow to export data to file

---
+++ backend/app/controllers/exporter.py
@@ -0,0 +1,47 @@
+from ..api import _v1
+from ..components.exporter import componentName
+from flask import session, render_template, request, redirect, url_for
+from flask_restful import Resource, Api
+from app.components._components import Components
+from app.components._plugins import ExporterPlugins
+from app.error import Error
+from flask_jsonpify import jsonify
+from app.utils.encoders import dataclassToJson
+from flask import send_file
+
+class ExporterController(Resource):
+    def __init__(self):
+        print("__init__")
+
+    # Obtiene la configuración del componente y sus plugins
+    def get(self):
+        components: Components = _v1._private.container[Components]
+        dataPlugins: ExporterPlugins = _v1._private.container[ExporterPlugins]
+        component = next(x for x in components if x.name == componentName )
+        cj = dataclassToJson(component)
+        cp = dataclassToJson(dataPlugins)
+        print(dataPlugins)
+        cj["plugins"] = cp
+        return jsonify(cj)
+
+    # Método que ejecuta el componente o un plugin del componente
+    def post(self):
+        data = None
+        components: Components = _v1._private.container[Components]
+        plugins: Components = _v1._private.container[ExporterPlugins]
+        pluginName = request.args.get('plugin')
+        if pluginName == None:
+            component = next(x for x in components if x.name == componentName )
+            data = _v1._private.container[component.handler_class](request)
+        else:
+            plugin = None
+            try:
+                plugin = next(x for x in plugins if x.name == pluginName )
+            except:
+                raise Error('El plugin {} no existe o no se ha instalado'.format(pluginName))
+            data = _v1._private.container[plugin.handler_class](request)
+        result = send_file(data['file'], as_attachment=True,
+                        attachment_filename=data['filename'],
+                        mimetype=data['mimetype'])
+        result.headers["File-Name"] = data['filename']
+        return result
