f294c5bd46f5d92d0ace1f5c6cdf6f90cfe3832e
José Manuel <parra.martin.josemanuel@gmail.com>
2021-04-04 22:28:04 +0200

    [ADD] paginator controls && paginator in a separated component

--- frontend/src/app/ex-datatable/ex-datatable/ex-datatable.component.ts
+++ frontend/src/app/ex-datatable/ex-datatable/ex-datatable.component.ts
@@ -17,6 +17,8 @@ export class ExDatatableComponent implements OnInit {
   @Input()
   menuItems: any[] = [];
 
+  index: number = 0;
+
   constructor() {}
 
   onCellDoubleClick() {
--- frontend/src/app/ex-datatable/ex-paginator/ex-paginator.component.ts
+++ frontend/src/app/ex-datatable/ex-paginator/ex-paginator.component.ts
@@ -6,7 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ex-paginator.component.scss'],
 })
 export class ExPaginatorComponent implements OnInit {
+  blockSize: number = 100;
+  firstRow: number = 1;
+  lastRow: number = 100;
+  totalRows: number = undefined;
+  totalPages: number = undefined;
+  page: number = 1;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  calcFirstLastRows(): void {
+    this.lastRow = this.page * this.blockSize;
+    this.firstRow = (this.page - 1) * this.blockSize + 1;
+  }
+
+  next(): void {
+    if (this.totalPages !== undefined) {
+      if (this.page < this.totalPages) {
+        this.page++;
+      }
+    } else {
+      this.page++;
+    }
+    this.calcFirstLastRows();
+  }
+
+  previous(): void {
+    if (this.page > 1) {
+      this.page--;
+    }
+    this.calcFirstLastRows();
+  }
+
+  first(): void {
+    this.page = 1;
+    this.calcFirstLastRows();
+  }
+  last(): void {
+    if (this.totalPages !== undefined) {
+      this.page = this.totalPages;
+      this.calcFirstLastRows();
+    }
+  }
 }
